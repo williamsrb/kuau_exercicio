cd6c2a21402aaf9c560d4bfceeae78dee00df001
Williams Ramos <ff9will@gmail.com>
2018-05-08 09:06:35 -0300

    Design

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,11 @@
+/* Biblioteca */
 
+/*  DOM carregado */
+document.addEventListener("DOMContentLoaded", function() {
+	// Configurações comportamentais
+});
+
+/* Recursos carregados */
+window.addEventListener("load", function() {
+	// Configurações visuais
+});
